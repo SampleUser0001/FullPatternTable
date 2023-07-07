65ab62320ea6b0e38af88d4c584324e18176200b
SampleUser0001 <ittimfn+github@gmail.com>
2023-07-07 22:09:14 +0900

    tmp

--- src/test/java/ittimfn/tool/fullpatterntable/controller/ConvertControllerTest.java
+++ src/test/java/ittimfn/tool/fullpatterntable/controller/ConvertControllerTest.java
@@ -3,6 +3,7 @@ package ittimfn.tool.fullpatterntable.controller;
 import java.util.List;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -17,8 +18,12 @@ public class ConvertControllerTest {
         testComponent = new ConvertController();
     }
     
+    /**
+     * isFinishのテスト
+     * 要素数 : 1, index : 未到達
+     */
     @Test
-    public void isFinish_not_enough_is_false() {
+    public void isFinish_not_enough_is_false_single() {
         List<List<String>> argsList = new ArrayList<List<String>>();
         List<String> list = new ArrayList<String>();
         list.add("hoge");
@@ -31,5 +36,47 @@ public class ConvertControllerTest {
         
         assertThat(testComponent.isFinish(), is(false));
     }
+
+    /**
+     * isFinishnテスト
+     * 要素数 : 1, index : 到達
+     */
+    @Test
+    public void isFinish_enough_is_true_single() {
+        List<List<String>> argsList = new ArrayList<List<String>>();
+        List<String> list = new ArrayList<String>();
+        list.add("hoge");
+        argsList.add(list);
+        testComponent.setArgsItemList(argsList);
+        
+        int[] indexs = new int[1];
+        indexs[0] = 1;
+        testComponent.setIndexs(indexs);
+        
+        assertThat(testComponent.isFinish(), is(true));
+    }
+
+    /**
+     * isFinishnテスト
+     * 要素数 : 1, index : 超過
+     */
+    @Test
+    public void isFinish_over_is_true_single() {
+        List<List<String>> argsList = new ArrayList<List<String>>();
+        List<String> list = new ArrayList<String>();
+        list.add("hoge");
+        argsList.add(list);
+        testComponent.setArgsItemList(argsList);
+        
+        int[] indexs = new int[1];
+        indexs[0] = 2;
+        testComponent.setIndexs(indexs);
+        
+        assertThat(testComponent.isFinish(), is(true));
+    }
     
+    @Test
+    public void failTest() {
+        fail("fail Test");
+    }
 }
